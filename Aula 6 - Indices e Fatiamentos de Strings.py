daf653cb3f9c0c26dd8efdf1ead50d2464b052c8
Jonatas <jonatasvieir@gmail.com>
2022-05-31 21:57:01 -0300

    Aula 6

---
+++ Aula 6 - Indices e Fatiamentos de Strings.py
@@ -0,0 +1,19 @@
+"""
+Manipulando strings - Aula 6
+* String indices
+* Fatiamento de strings (inicio:fim:passo)
+*Funções built-in len, abs, type, print, etc...
+Essas funções podem ser usadas diretamente em cada tipo.
+
+Você pode conferir tudo isso em:
+https://docs.python.org/3/library/stdtypes.html
+https://docs.python.org/3/library/functions.html
+
+"""
+
+# positivos      [012345678]
+texto          = 'Python_s2'
+# negativos     -[987654321]
+
+print( len(texto))
+
